f58fd2a0b8482ecd4d160ca5c4268b8029f78960
runeteamop <rune@team-op.dk>
2026-04-08 11:54:17 +0200

    Merged with dev

---
+++ singleton/scene_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var current_scene: Node
+
+var main_menu: String = "uid://cdwkb8h2hb2wy"
+var	sub_menu: String = "uid://dmg2j14j7w1eb"
+
+func change_scene(scene: String) -> void:
+	if current_scene != null:
+		remove_child(current_scene)
+	
+	var instantiated_scene: Node = load(scene).instantiate()
+	
+	add_child(instantiated_scene)
+	current_scene = instantiated_scene
