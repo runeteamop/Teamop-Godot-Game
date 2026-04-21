941cfdbc1afd9ec59ab6b28ae0d2ce8a4d90c70b
runeteamop <rune@team-op.dk>
2026-04-21 10:42:54 +0200

    removed the enemys target

--- bin/entities/enemy.gd
+++ bin/entities/enemy.gd
@@ -2,14 +2,10 @@ class_name Enemy extends Area3D
 
 const SPEED = 3
 
-var target
-
 func _physics_process(delta: float) -> void:
 	if Player.instance:
-		target = Player.instance.position
-		look_at(target)
+		look_at(Player.instance.position)
 		global_transform.origin -= transform.basis.z.normalized() * SPEED * delta
 
 func _hit() -> void:
-	print("hey")
 	queue_free()
