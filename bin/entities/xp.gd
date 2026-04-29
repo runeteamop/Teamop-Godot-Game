136eb21238db21dca75aaa75de69868f41b4928f
runeteamop <rune@team-op.dk>
2026-04-29 13:30:17 +0200

    made the xp go towards the player when close enough

--- bin/entities/xp.gd
+++ bin/entities/xp.gd
@@ -1,9 +1,19 @@
 class_name Xp extends Area3D
 
+var player: CharacterBody3D
+var speed: float = 2
+
 func _physics_process(_delta: float) -> void:
 	rotate_y(0.05)
+	if player:
+		var player_pos = player.position
+		global_position = global_position.move_toward(player_pos, _delta * speed)
+		speed += 0.1
+		
+		if global_position == player_pos:
+			Player_values.xp += 1
+			queue_free()
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
-		Player_values.xp += 1
-		queue_free()
+		player = body
