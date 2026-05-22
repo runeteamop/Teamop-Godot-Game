044ce6aa1d92637b0969462171786dc8f4394f84
runeteamop <rune@team-op.dk>
2026-05-22 13:18:19 +0200

    changed globals emits to so they emit from a function in global

--- bin/autoload/global.gd
+++ bin/autoload/global.gd
@@ -1,10 +1,7 @@
 extends Node
 
-
 signal request_game_state(file_path: String)
-@warning_ignore("unused_signal")
 signal enter_menu(file_path: String)
-@warning_ignore("unused_signal")
 signal goto_last_menu
 signal flush_menu_stack
 
@@ -12,3 +9,11 @@ func enter_game_state(file_path: String) -> void:
 	emit_signal("flush_menu_stack")
 	emit_signal("request_game_state", file_path)
 	get_tree().paused = false
+
+func emit_request_game_state(file_path: String) -> void: request_game_state.emit(file_path)
+
+func emit_enter_menu(file_path: String) -> void: enter_menu.emit(file_path)
+
+func emit_goto_last_menu(file_path: String) -> void: goto_last_menu.emit(file_path)
+
+func emit_flush_menu_stack(file_path: String) -> void: flush_menu_stack.emit(file_path)
