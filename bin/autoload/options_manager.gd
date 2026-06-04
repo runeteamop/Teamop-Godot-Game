c7f76cf452a73f4dcd8ea4e7a58469b5f755721c
Absolute-Kino <asger@team-op.dk>
2026-06-04 14:07:25 +0200

    ui scale

--- bin/autoload/options_manager.gd
+++ bin/autoload/options_manager.gd
@@ -12,12 +12,14 @@ func _init() -> void:
 	else:
 		options_file.set_value("display", "window_mode", 3)
 		options_file.set_value("display", "resolusion_scale", 1.0)
+		options_file.set_value("display", "ui_scale", 1.0)
 
 		save_options()
 
 func apply_all_options() -> void:
 	DisplayServer.window_set_mode(options_file.get_value("display", "window_mode"))
 	get_viewport().scaling_3d_scale = options_file.get_value("display", "resolusion_scale")
+	get_window().set_content_scale_size(Vector2i(7680.0, 4320.0) * float(2.0 - options_file.get_value("display", "ui_scale")))
 
 func options_to_buffer(key: String, subkey: String, value: Variant) -> void:
 	options_buffer[subkey] = func() -> void: options_file.set_value(key, subkey, value)
--- bin/ui/options_menu.gd
+++ bin/ui/options_menu.gd
@@ -4,6 +4,7 @@ class_name OptionsMenu extends Control
 const RESOLUSIONS: Array[Vector2] = [Vector2(1280, 720), Vector2(1920, 1080), Vector2(2560, 1440), Vector2(3840, 2160)]
 const RESOLUSION_SCALE_VALUES: Array[float] = [0.5, 1.0, 2.0]
 
+@export var test: Label
 @export var fullscreen_toggle_button: CheckBox
 @export var resolusion_scale_dropdown: OptionButton
 @export var resolusions: OptionButton
@@ -15,8 +16,9 @@ var applied: bool = false
 func _enter_tree() -> void:
 	fullscreen_toggle_button.button_pressed = true if OptionsManager.options_file.get_value("display", "window_mode", 3) else false
 	resolusion_scale_dropdown.selected = RESOLUSION_SCALE_VALUES.find(OptionsManager.options_file.get_value("display", "resolusion_scale"))
-	resolusions.selected = RESOLUSIONS.find(get_viewport().get_visible_rect().size)
-	
+	#resolusions.selected = RESOLUSIONS.find(get_viewport().get_visible_rect().size)
+	print(get_viewport().get_visible_rect().size)
+
 	print(get_viewport().get_visible_rect().size)
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
@@ -33,12 +35,8 @@ func _on_back_pressed() -> void:
 func _on_apply_pressed() -> void:
 	OptionsManager.save_options()
 	OptionsManager.apply_all_options()
-	
-func _on_option_button_2_item_selected(index: int) -> void:
-	get_window().set_size(RESOLUSIONS[index])
-	#DisplayServer.window_set_position(DisplayServer.screen_get_size(DisplayServer.window_get_current_screen()) * 2)
-	var t = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
-	var e = get_window().get_size_with_decorations()
-	print(t-e)
-	get_window().set_position(t-e/2)
-	
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	get_window().set_content_scale_size(Vector2i(7680.0, 4320.0) * float(2.0 - value))
+	test.text = "%d%%" % int(value * 100)
